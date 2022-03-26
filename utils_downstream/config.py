c542730cd965a0aa3925c44598e2534500fe6cce
赵骁骐 <834081007@qq.com>
2022-03-26 20:26:10 +0800

    Add files via upload

---
+++ utils_downstream/config.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+import os
+
+dutrgbd_root_test = ''
+njud_root_test = ''
+nlpr_root_test = ''
+stere_root_test =''
+sip_root_test = ''
+rgbd135_root_test = ''
+ssd_root_test = ''
+lfsd_root_test = ''
+
+
+dutrgbd = os.path.join(dutrgbd_root_test)
+njud = os.path.join(njud_root_test)
+nlpr = os.path.join(nlpr_root_test)
+stere = os.path.join(stere_root_test)
+sip = os.path.join(sip_root_test)
+rgbd135 = os.path.join(rgbd135_root_test)
+ssd = os.path.join(ssd_root_test)
+lfsd = os.path.join(lfsd_root_test)
+
+
+SSLSOD = '' # model
+
+RGBD_SOD_Models = {'SSLSOD':SSLSOD}
